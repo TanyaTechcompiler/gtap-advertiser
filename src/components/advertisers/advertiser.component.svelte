1a60c58fd5c0c80ea9234a4d6c2e32b359d9d46b
Tanya Singhal <tanyasinghal16@outlook.com>
2022-06-11 17:16:00 +0530

    adv layout

--- src/components/advertisers/advertiser.component.svelte
+++ src/components/advertisers/advertiser.component.svelte
@@ -1,18 +1,22 @@
+<script>
+    import { AccordionItem } from "flowbite-svelte";
+  </script>
+
 <AccordionItem id="1">
     <h2 slot="header">My Header 1</h2>
     <div slot="body">
-      <p class="mb-2 text-gray-500 dark:text-gray-400">
-        Lorem ipsum dolor sit amet ...
-      </p>
-      ...
+        <p class="mb-2 text-gray-500 dark:text-gray-400">
+            Lorem ipsum dolor sit amet ...
+        </p>
+        ...
     </div>
-  </AccordionItem>
-  <AccordionItem id="2">
+</AccordionItem>
+<AccordionItem id="2">
     <h2 slot="header">My Header 2</h2>
     <div slot="body">
-      <p class="mb-2 text-gray-500 dark:text-gray-400">
-        Lorem ipsum dolor sit amet ...
-      </p>
-      ...
+        <p class="mb-2 text-gray-500 dark:text-gray-400">
+            Lorem ipsum dolor sit amet ...
+        </p>
+        ...
     </div>
-  </AccordionItem>
+</AccordionItem>
